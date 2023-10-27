1e797cf3eaa876dfc5200cf8c52e841234c1e0d8
Roycy Eduardo <eduardo.roycy@gmail.com>
2023-10-27 09:30:18 -0300

    Add files via upload

--- src/js/bloquearCoisas.js
+++ src/js/bloquearCoisas.js
@@ -1,9 +1,3 @@
 document.oncontextmenu = function() {
     return false;
 }
-
-document.onkeydown = function(event) {
-    if (event.keyCode >= 112 && event.keyCode <= 123) {
-        return false;
-    }
-}
